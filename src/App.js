dfa604c9f3db605a1ca1f903992192bc2e7fd672
Elohor-Grace <omorogunelohor@gmail.com>
2021-08-07 12:06:04 +0100

    week10 text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import About from './Component/About'
+import Contact from './Component/Contact'
+import Home from './Component/Home'
+import Login from './Component/Login'
 
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <nav>
+       <Link to ="/home page">HOME </Link>
+       <Link to ="/about">ABOUT</Link>
+       <Link to ="/contact">CONTACT</Link>
+       <Link to ="/login">LOGIN</Link>
+       </nav> 
+      
+       
+       <Switch>
+         <Route path ="/about">
+           <About />
+         </Route>
+
+         <Route path ="/contact">
+           <Contact />
+         </Route>
+
+         <Route path ="/Login">
+           <Login />
+         </Route>
+
+         <Route path ="/">
+           <Home />
+         </Route>
+       </Switch>
+
     </div>
   );
 }
